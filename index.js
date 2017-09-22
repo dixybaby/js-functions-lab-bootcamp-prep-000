4ab01b1509e6f0033b2ba8428537c68ffac2a998
Barbara Dixon <babsnwosisi@yahoo.com>
2017-09-22 15:28:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,13 +4,6 @@ function happyHolidays(string) {
 }
 
 function happyHolidaysTo(name) {
+  console.log("${name}!")
   return "Happy holidays, you!"
 }
-
-function happyHolidayTo(holiday, name) {
-  return "Happy Independence Day, you!"
-}
-
-function holidayCountdown(holiday, days) {
-  return "It's 20 days until Mother's Day!"
-      }
