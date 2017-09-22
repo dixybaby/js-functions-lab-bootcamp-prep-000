aa1285119a4bad9034511ace7a826be291536e0b
Barbara Dixon <babsnwosisi@yahoo.com>
2017-09-22 15:32:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ function happyHolidaysTo(name) {
   console.log("${name}!")
   return "Happy holidays, you!"
 }
+
+function happyHolidayTo(holiday, name) {
+  console.log("${holiday}, ${name}!")
+  return "Happy Independece Day, you!"
+}
